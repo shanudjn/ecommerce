c5d0592fbadd1e747f19dd44759416d3da76cc5e
Shahazad <shanudjn@gmail.com>
2021-04-27 13:56:06 +0000

    process env

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const username = process.env['username'];
 const password = process.env['password'];
+const username = process.env['username']
 
 async function initializeDBConnection(){
-  const uri = 'mongodb+srv://'+username+':'+password+'@neog-cluster.7up1q.mongodb.net/ecommerce?retryWrites=true&w=majority'
+  const uri = `mongodb+srv://${usename}:${password}@neog-cluster.7up1q.mongodb.net/ecommerce?retryWrites=true&w=majority`
   try{
     const connectionResponse = await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
