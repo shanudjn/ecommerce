25299552b602fed51f03c386b581359f83acc00f
Shahazad <shanudjn@gmail.com>
2021-04-27 14:00:22 +0000

    changed format of password

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const password = process.env['password'];
-const username = encodeURIComponent(process.env['username'])
+const username = process.env['username'];
+const password = encodeURIComponent(process.env['password']);
 
 async function initializeDBConnection(){
   const uri = `mongodb+srv://${username}:${password}@neog-cluster.7up1q.mongodb.net/ecommerce?retryWrites=true&w=majority`
