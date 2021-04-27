45296e30f5641f6f71fd44c2a347625c862ca54e
Shahazad <shanudjn@gmail.com>
2021-04-27 14:14:37 +0000

    encoded password

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const username = process.env['username'];
-const password = encodeURIComponent(process.env['password']);
+const password = process.env['password'];
 
 async function initializeDBConnection(){
   const uri = `mongodb+srv://${username}:${password}@neog-cluster.7up1q.mongodb.net/ecommerce?retryWrites=true&w=majority`
